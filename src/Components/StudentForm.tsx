a5a15c01e91783f4f6e26942512ff66d5ff3b92e
HopKristian <kh1811kodehode@gmail.com>
2025-02-27 13:41:01 +0100

    StudentForm NOT working. need to do JSON.stringify in try catch

--- src/Components/StudentForm.tsx
+++ src/Components/StudentForm.tsx
@@ -1,9 +1,25 @@
+import {useState} from "react";
+
 const StudentForm = () => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [age, setAge] = useState(0);
+
     const AddStudentToTable = async () =>
     {
+        
+
+
         const url = "http://localhost:5024/students/addstudent";
         try {
-            const response = await fetch(url);
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify()
+            });
             const data = await response.json();
             console.log(data);
 
@@ -17,14 +33,14 @@ const StudentForm = () => {
     return (
         <div className="d-flex flex-column align-items-center justify-content-center gap-2 mb-3" id="form-group">
             <label htmlFor="name">Student Name</label>
-            <input type="text" id="name" placeholder="Start writing..."></input>
+            <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Start writing..."></input>
 
             <label>Student Email</label>
-            <input type="text" id="email" placeholder="Start writing..."></input>
+            <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Start writing..."></input>
 
             <label>Student Age</label>
-            <input type="number" id="age" placeholder="Start writing..."></input>
-            <button className="btn btn-secondary" type="submit">Find</button>
+            <input type="number" id="age" value={age} onChange={(e) => setAge(Number(e.target.value))} placeholder="Start writing..."></input>
+            <button className="btn btn-secondary" type="submit" onClick={AddStudentToTable}>Find</button>
         </div>
     )
 }
